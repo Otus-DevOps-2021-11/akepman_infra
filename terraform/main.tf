199656cc0717e539f8a6ffd686b8fd2f62f7e155
akepman <a@kepman.ru>
2022-01-26 23:03:05 +0300

    terra first3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,20 @@
-provider "google" {
-  #Provider version
-  version = "2.0.0"
-
-  project = "${var.project}"
-  region  = "${var.region}"
+provider "yandex" {
+  token     = "AQAAAAAHEVvoAATuwbdPijQW9kInhfSKpjpHKcE"
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
 }
 
 module "app" {
-  source          = "../modules/app"
-  public_key_path = "${var.public_key_path}"
-  private_key     = "${var.private_key}"
-  zone            = "${var.zone}"
-  app_disk_image  = "${var.app_disk_image}"
-  db_ip           = "${module.db.internal_ip}"
+  source          = "./modules/app"
+  public_key_path = var.public_key_path
+  app_disk_image  = var.app_disk_image
+  subnet_id       = var.subnet_id
 }
 
 module "db" {
-  source          = "../modules/db"
-  public_key_path = "${var.public_key_path}"
-  private_key     = "${var.private_key}"
-  zone            = "${var.zone}"
-  db_disk_image   = "${var.db_disk_image}"
-}
-
-module "vpc" {
-  source = "../modules/vpc"
-
-  source_ranges = ["90.154.71.173/32"]
+  source          = "./modules/db"
+  public_key_path = var.public_key_path
+  db_disk_image   = var.db_disk_image
+  subnet_id       = var.subnet_id
 }
